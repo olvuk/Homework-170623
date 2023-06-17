a778a4618d385219f569aa4232add3b0e600ee27
Olga <radenovic-family@yandex.ru>
2023-06-17 12:50:03 +0200

    Added task 19

---
+++ task19/Program.cs
@@ -0,0 +1,36 @@
+﻿int inputNumber(string message)
+{
+    Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void checkNumber(int number)
+{
+    if (number > 9999 && number < 100000)
+    {
+        int num5 = number % 10;
+        int num4 = (number / 10) % 10;
+        int num3 = (number / 100) % 10;
+        int num2 = (number / 1000) % 10;
+        int num1 = (number / 10000) % 10;
+
+        if (num1 == num5 && num2 == num4)
+        {
+            Console.WriteLine(number + " является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine(number + " не является палиндромом");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не пятизначное число");
+    }
+
+}
+
+int number = inputNumber("Введите, пожалуйста, пятизначное число");
+checkNumber(number);
+
