f2def9c7b43f0d481eb98966f125186155326a58
Olga <radenovic-family@yandex.ru>
2023-06-17 15:19:09 +0200

    Added task 23

---
+++ task23/Program.cs
@@ -0,0 +1,30 @@
+﻿int inputNumber(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void getNumberCubed(int number)
+{
+    int index = 1;
+    double result = 0;
+    while (index <= number)
+    {
+        result = Math.Pow(index, 3);
+        index++;
+        
+        if (index != number + 1)
+        {
+            Console.Write(result + ", ");
+        }
+        else
+        {
+            Console.Write(result + ".");
+        }
+    }
+
+}
+
+int number = inputNumber("Введите, пожалуйста, число");
+getNumberCubed(number);
