36e0f6ace0d384904ab0b907e2c760aff97cdf81
Olga <radenovic-family@yandex.ru>
2023-06-17 13:02:06 +0200

    Added task 21

---
+++ task21/Program.cs
@@ -0,0 +1,22 @@
+﻿int inputNumber(string message)
+{
+    Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+double findDistance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+    return distance;
+}
+
+int x1 = inputNumber("Введите, пожалуйста, координату х точки А");
+int y1 = inputNumber("Введите, пожалуйста, координату y точки А");
+int z1 = inputNumber("Введите, пожалуйста, координату z точки А");
+int x2 = inputNumber("Введите, пожалуйста, координату х точки B");
+int y2 = inputNumber("Введите, пожалуйста, координату y точки B");
+int z2 = inputNumber("Введите, пожалуйста, координату z точки B");
+
+double distanceAB = findDistance(x1, y1, z1, x2, y2, z2);
+Console.WriteLine(distanceAB);
